ae3b44d3bd829dba21777ef033af4328e859c069
Belle (Laptop) <nightofthebuttlickers@gmail.com>
2025-10-29 19:32:15 -0500

    optimize assignRecursive option check

--- util/recursive.js
+++ util/recursive.js
@@ -6,22 +6,14 @@ export function recur(callback, ...params) {
 }
 
 export function assignRecursive(obj, that, options) {
-	if (options?.reassign ?? true)
-		recur((iterate, entries, that) => {
-			for (const [key, value] of entries) {
-				if (value.constructor.name === "Object") {
-					that[key] ??= {};
-					iterate(Object.entries(value), that[key]);
-				} else that[key] = value;
-			}
-		}, Object.entries(obj), that);
-	else
-		recur((iterate, entries, that) => {
-			for (const [key, value] of entries) {
-				if (value.constructor.name === "Object") {
-					that[key] ??= {};
-					iterate(Object.entries(value), that[key]);
-				} else that[key] ??= value;
-			}
-		}, Object.entries(obj), that);
+	recur((iterate, entries, that) => {
+		for (const [key, value] of entries) {
+			if (value.constructor.name === "Object") {
+				that[key] ??= {};
+				iterate(Object.entries(value), that[key]);
+			} else
+				if (options?.reassign ?? true) that[key] = value;
+				else that[key] ??= value;
+		}
+	}, Object.entries(obj), that);
 }
